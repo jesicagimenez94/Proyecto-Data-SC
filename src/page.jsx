d67217e4fa65283f59675b3442f03ae8309b7c94
Joel Barrientos <Barrientoss.joel@gmail.com>
2025-04-23 13:17:31 -0300

    Actualiza dependencias y reestructura componentes: se actualiza "react-hook-form" a la versión 7.56.0, se eliminan secciones de "DesktopViewSection" y "ServicesSection", y se añade el componente "Home" en "page.jsx" con nuevas secciones.

---
+++ src/page.jsx
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////
+// Importaciones
+import HeroSection from "./components/sections/HeroSection";
+import ExperienceSection from "./components/sections/ExperienceSection";
+import {ServicesSection} from "./components/sections/services/servicesSection";
+import TeamSection from "./components/sections/TeamSection";
+import StatsSection from "./components/sections/StatsSection";
+import TestimonialSection from "./components/sections/TestimonialSection";
+import BenefitsSection from "./components/sections/BenefitsSection";
+import Proyectos from "./components/sections/Proyectos";
+import FormularioSection from "./components/sections/FormSection";
+//import CTASection from "./components/sections/CTASection";
+
+//////////////////////////////////////////////////////////
+// Componente Home (DesktopViewSection)
+export const Home = () => {
+  return (
+    <div className="flex flex-col items-start w-full">
+      <HeroSection />
+      <ServicesSection />
+      <ExperienceSection />
+      <BenefitsSection />
+      <TestimonialSection />
+      <TeamSection />
+      <Proyectos />
+      <StatsSection />
+      {/*<CTASection />*/}
+      <FormularioSection />
+    </div>
+  );
+};
